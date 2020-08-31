e0b8c59aead75ded80ce997d5fbe49103798b6eb
Sahresh Waheed <sehrishwaheed98@gmail.com>
2020-09-01 00:02:14 +0300

    Popular books

--- js/script.js
+++ js/script.js
@@ -12,5 +12,13 @@ $(document).ready(function(){
           }else{
             $('.gallery').removeClass('change');
           }
+      });
+
+      $('.writers-accordion').click(function(event){
+
+        if(event.target.id.split('-')[0] === 'button'){
+            $('#book-1').attr('src', '/img/writers/'+ event.target.id.split('-')[1]+ '-book1.jpg')
+            $('#book-2').attr('src', '/img/writers/'+ event.target.id.split('-')[1]+ '-book2.jpg')
+        }
       })
 })
