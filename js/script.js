6c2e4959e5494302b77d216d0f6facd0da2be33a
Sahresh Waheed <sehrishwaheed98@gmail.com>
2020-09-01 16:01:44 +0300

    testing

--- js/script.js
+++ js/script.js
@@ -22,8 +22,8 @@ $(document).ready(function(){
       $('.writers-accordion').click(function(event){
 
         if(event.target.id.split('-')[0] === 'button'){
-            $('#book-1').attr('src', '/img/writers/'+ event.target.id.split('-')[1]+ '-book1.jpg')
-            $('#book-2').attr('src', '/img/writers/'+ event.target.id.split('-')[1]+ '-book2.jpg')
+            $('#book-1').attr('src', 'img/writers/'+ event.target.id.split('-')[1]+ '-book1.jpg')
+            $('#book-2').attr('src', 'img/writers/'+ event.target.id.split('-')[1]+ '-book2.jpg')
         }
       })
 })
